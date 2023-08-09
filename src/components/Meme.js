435bf47074b62c10d11c825ad28ce91bd2f3300d
Krishna Kamath <kamathkrishn16@gmail.com>
2023-08-09 09:26:04 +0530

    Images added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,29 @@
 import React from "react";
+import memesData from '../memesData';
 
 function Meme() {
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "",
+            bottonText: "",
+            randomImage: ""
+        }
+    )
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url =  memesArray[randomNumber].url
+        setMeme(prevMeme => (
+            {
+                ...prevMeme,
+                randomImage: url
+            }
+        ))    
+    }
+
     return (
         <main>
             <form className="form">
@@ -16,10 +39,13 @@ function Meme() {
                 />
                 <button 
                     className="form--button"
+                    type="button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
             </form>
+            <img src = {meme.randomImage} alt = "MemeImage" className = "meme--image"/>
         </main>
     )
 }
